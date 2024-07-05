e43df550c42bf4cea7cf7553a0c57d7e920a7c9f
Madhesh <madhesh.git@gmail.com>
2024-07-05 22:01:17 +0530

    update gitignore

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCv3-XJEQA5HCt6rSYdcuka-PhHvyDUeEI",
+  authDomain: "cryptoplace-78d2d.firebaseapp.com",
+  projectId: "cryptoplace-78d2d",
+  storageBucket: "cryptoplace-78d2d.appspot.com",
+  messagingSenderId: "211610860000",
+  appId: "1:211610860000:web:e039b6dbbd90c01be05385",
+};
+
+
+const app = initializeApp(firebaseConfig);
